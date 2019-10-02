6b760cc71ba00335a0ad41c8128ed7e645debbc6
Adarsh <adarshkumar124@gmail.com>
2019-10-02 13:52:53 +0530

    First

---
+++ Assets/Scripts/TileScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Assets/ScriptableObjects/Tile", menuName = "ScriptableObjects/Tile")]
+public class TileScriptableObject : ScriptableObject
+{
+    //public TileType tileType;
+    //public GameObject tankBody;
+    public string TileName;
+}
+
+[CreateAssetMenu(fileName = "Assets/ScriptableObjects/TileScriptableObjectList", menuName = "ScriptableObjects/Tile Lists")]
+public class TileScriptableObjectList : ScriptableObject
+{
+    public TileScriptableObject[] tiles;
+}
